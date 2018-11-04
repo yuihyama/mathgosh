6ab50113dcb5dba65dec97474066b3c6b076c902
yuihyama <yuihyama@gmail.com>
2018-11-04 09:00:42 +0900

    Update main.scm

--- main.scm
+++ main.scm
@@ -1,13 +1,16 @@
 #!/usr/bin/env gosh
 
+;; Write your code here ...
+
 (load "./add/my-add")
 (load "./add/my-add-lambda")
+(load "./add/add")
 
-(display (my-add 1 2))
-(print)
-(print (my-add 1 2))
-(print)
+(display (my-add 1 2)) ;; 3
+(newline)
 
-(display (my-add-lambda 1 2))
-(print)
-(print (my-add-lambda 1 2))
+(display (my-add-lambda 1 2)) ;; 3
+(newline)
+
+(display (add 1 2)) ;; 3
+(newline)
