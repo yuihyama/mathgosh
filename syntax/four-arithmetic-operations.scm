66019c5f569302412495f3b219b862513cb29325
yuihyama <yuihyama@gmail.com>
2018-11-07 00:04:17 +0900

    Add four-arithmetic-operations.scm

---
+++ syntax/four-arithmetic-operations.scm
@@ -0,0 +1,63 @@
+#!/usr/bin/env gosh
+
+(display (+ 10 5) )
+(newline)
+
+(display (- 10 5) )
+(newline)
+
+(display (* 10 5) )
+(newline)
+
+(display (/ 10 5) )
+(newline)
+
+(display (+ 1 2 3) )
+(newline)
+
+(display (- 1 2 3) )
+(newline)
+
+(display (* 1 2 3) )
+(newline)
+
+(display (/ 1 2 3) )
+(newline)
+
+(display (+ (+ 1 2) 3) )
+(newline)
+
+(display (- (- 1 2) 3) )
+(newline)
+
+(display (* (* 1 2) 3) )
+(newline)
+
+(display (/ (/ 1 2) 3) )
+(newline)
+
+(display (+ 1 2 3 4 5 6 7 8 9) )
+(newline)
+
+(display (- 1 2 3 4 5 6 7 8 9 ) )
+(newline)
+
+(display (* 1 2 3 4 5 6 7 8 9 ) )
+(newline)
+
+(display (/ 1 2 3 4 5 6 7 8 9) )
+(newline)
+
+(display (- (+ (/ 1 2) (* 3 4)) 5) )
+(newline)
+
+(display (-
+           (+
+             (/ 1 2)
+             (* 3 4)
+           )
+          5) )
+(newline)
+
+(display (* 1 (* 2 (* 3 (* 4 (* 5 6) )))) )
+(newline)
